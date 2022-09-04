0d63ad673b0a3dea8e0eab3cc586c1b71df54e89
Mat Krol <43745362+mat-krol@users.noreply.github.com>
2022-09-04 22:45:54 +0100

    extract the filter util out of the main script body

--- src/scripts/postSiteOutages.ts
+++ src/scripts/postSiteOutages.ts
@@ -2,6 +2,7 @@ import { Outage } from "../types/Outage";
 import { SiteInfo } from "../types/SiteInfo";
 import { fetchOutages } from "../utils/fetchOutages";
 import { fetchSiteInfo } from "../utils/fetchSiteInfo";
+import { filterAndEnrichSiteOutages } from "./utils/filterAndEnrichSiteOutages";
 import { sendSiteOutages } from "../utils/sendSiteOutages";
 
 const siteId = "norwich-pear-tree";
@@ -12,23 +13,13 @@ async function postSiteOutages() {
     const outages: Outage[] = await fetchOutages();
     const siteInfo: SiteInfo = await fetchSiteInfo();
 
-    const siteDeviceIds = siteInfo.devices.map((device) => device.id);
+    const siteOutages = filterAndEnrichSiteOutages({
+      outages,
+      siteInfo,
+      startDate,
+    });
 
-    const enrichedFilteredOutages = outages.reduce(
-      (filteredOutages: Outage[], outage) => {
-        if (outage.begin > startDate && siteDeviceIds.includes(outage.id)) {
-          const deviceName = siteInfo.devices.find(
-            (device) => device.id === outage.id
-          )!.name;
-          return [...filteredOutages, { ...outage, name: deviceName }];
-        } else {
-          return filteredOutages;
-        }
-      },
-      []
-    );
-
-    await sendSiteOutages(enrichedFilteredOutages);
+    await sendSiteOutages(siteOutages);
   } catch (error) {}
 }
 
---
+++ src/scripts/utils/filterAndEnrichSiteOutages.ts
@@ -0,0 +1,30 @@
+import { Outage } from "../../types/Outage";
+import { SiteInfo } from "../../types/SiteInfo";
+
+type Args = {
+  outages: Outage[];
+  siteInfo: SiteInfo;
+  startDate: string;
+};
+
+export const filterAndEnrichSiteOutages = ({
+  outages,
+  siteInfo,
+  startDate,
+}: Args) => {
+  const siteDeviceIds = siteInfo.devices.map((device) => device.id);
+
+  return outages.reduce((filteredOutages: Outage[], outage) => {
+    if (outage.begin > startDate && siteDeviceIds.includes(outage.id)) {
+      const deviceName = siteInfo.devices.find(
+        (device) => device.id === outage.id
+      )!.name;
+
+      const enrichedOutage = { ...outage, name: deviceName };
+
+      return [...filteredOutages, enrichedOutage];
+    }
+
+    return filteredOutages;
+  }, []);
+};
