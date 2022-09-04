3086b3bb80c21e417a63c5aa58a20e5da3aa28bd
Mat Krol <43745362+mat-krol@users.noreply.github.com>
2022-09-04 22:43:38 +0100

    flesh out the main script functionality

--- src/scripts/postSiteOutages.ts
+++ src/scripts/postSiteOutages.ts
@@ -1,6 +1,34 @@
+import { Outage } from "../types/Outage";
+import { SiteInfo } from "../types/SiteInfo";
+import { fetchOutages } from "../utils/fetchOutages";
+import { fetchSiteInfo } from "../utils/fetchSiteInfo";
+import { sendSiteOutages } from "../utils/sendSiteOutages";
+
+const siteId = "norwich-pear-tree";
+const startDate = "2022-01-01T00:00:00.000Z";
+
 async function postSiteOutages() {
   try {
-    console.log("posting outages");
+    const outages: Outage[] = await fetchOutages();
+    const siteInfo: SiteInfo = await fetchSiteInfo();
+
+    const siteDeviceIds = siteInfo.devices.map((device) => device.id);
+
+    const enrichedFilteredOutages = outages.reduce(
+      (filteredOutages: Outage[], outage) => {
+        if (outage.begin > startDate && siteDeviceIds.includes(outage.id)) {
+          const deviceName = siteInfo.devices.find(
+            (device) => device.id === outage.id
+          )!.name;
+          return [...filteredOutages, { ...outage, name: deviceName }];
+        } else {
+          return filteredOutages;
+        }
+      },
+      []
+    );
+
+    await sendSiteOutages(enrichedFilteredOutages);
   } catch (error) {}
 }
 
